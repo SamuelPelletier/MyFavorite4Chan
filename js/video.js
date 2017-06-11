df5157aa98abc6971ae4ecb6011c3fde7bf4021d
Samuel Pelletier <pellesam.info@gmail.com>
2017-06-11 20:21:44 +0200

    Add title

--- js/video.js
+++ js/video.js
@@ -2,11 +2,15 @@ var videos = ['miku_dance','miku_gun','impro','miku_russian','monkey', 'ayaya']
 
 $(document).ready(function(){
     videos.forEach(function(video) {
+        var title = video.replace("_", " ")
+        .toLowerCase().replace(/\b[a-z]/g, function(letter) {
+            return letter.toUpperCase();
+        });
         var url = "./video/"+video+".webm"
         $('.content').append(
         '<div class="row">'+
             '<div class="col-md-7">'+
-        '<h3>Miku Dance</h3>'+
+        '<h3>'+title+'</h3>'+
             '<video controls>'+
         '<source src='+url+'>'+
             '</video>'+
