d71d723c23aa1f98c4e125dae28b6b6d19bda41f
Samuel Pelletier <pellesam.info@gmail.com>
2017-06-22 21:02:53 +0200

    Add video shop and good man

--- js/video.js
+++ js/video.js
@@ -1,4 +1,4 @@
-var videos = ['escape_anime','nice_presenter','speed_car','life','russian_kid','miku_dance','miku_gun','impro','miku_russian','monkey', 'ayaya']
+var videos = ['good_man','shop','escape_anime','nice_presenter','speed_car','life','russian_kid','miku_dance','miku_gun','impro','miku_russian','monkey', 'ayaya']
 
 $(document).ready(function(){
     videos.forEach(function(video) {
